b6bf6f07fde1a1baddba099c9b8edb5d3a25b045
Sergey Bazlov <101872564+sab1982@users.noreply.github.com>
2022-06-01 11:37:40 +0400

    Update main.py

--- main.py
+++ main.py
@@ -40,8 +40,10 @@ def convert(message: telebot.types.Message):
 
         quote, base, amount = values
         total_base = CryptoConverter.get_price(quote, base, amount)
+        
     except APIException as e:
         bot.reply_to(message, f'Ошибка пользователя!\n{e}')
+        
     except Exception as e:
         bot.reply_to(message, f'Не удалось обработать команду\n{e}')
     else:
